6f9fb9f53e063f07cbfd3c472cbc49beef762651
VernonDodo <vcdodo@gmail.com>
2019-05-16 12:26:54 -0400

    Had to change the date format in the user model and seeds files

--- db/userSeeds.js
+++ db/userSeeds.js
@@ -1,37 +1,37 @@
 const User = require('../models/User')
 
     User.deleteMany().then(() => {
-        const jackie = new User({first_name:"Jackie", last_name: "Chan", date_of_birth: 01/02/1956, email: "jackiechan@gmail.com"})
+        const jackie = new User({first_name:"Jackie", last_name: "Chan", date_of_birth: 1956-02-01, email: "jackiechan@gmail.com"})
         return jackie.save()
     }).then(() => {
-        const james = new User({first_name:"James", last_name: "Brody", date_of_birth: 02/03/1957, email: "jamesbrody@gmail.com"})
+        const james = new User({first_name:"James", last_name: "Brody", date_of_birth: 1957-03-02, email: "jamesbrody@gmail.com"})
         return james.save()
     }).then(() => {
-        const ellen = new User({first_name:"Ellen", last_name: "Nefdt", date_of_birth: 03/03/1958, email: "enefdt@gmail.com"})
+        const ellen = new User({first_name:"Ellen", last_name: "Nefdt", date_of_birth: 1958-03-03, email: "enefdt@gmail.com"})
         return ellen.save()
     }).then(() => {
-        const lisa = new User({first_name:"Lisa", last_name: "Stark", date_of_birth: 04/12/1959, email: "lisa.stark@gmail.com"})
+        const lisa = new User({first_name:"Lisa", last_name: "Stark", date_of_birth: 1959-04-12, email: "lisa.stark@gmail.com"})
         return lisa.save()
     }).then(() => {
-        const ashleigh = new User({first_name:"Ashleigh", last_name: "Tisdale", date_of_birth: 01/02/1986, email: "atisdale@gmail.com"})
+        const ashleigh = new User({first_name:"Ashleigh", last_name: "Tisdale", date_of_birth: 1986-02-01, email: "atisdale@gmail.com"})
         return ashleigh.save()
     }).then(() => {
-        const ashley = new User({first_name:"Ashley", last_name: "Adonis", date_of_birth: 05/04/1991, email: "ashley.adonis@gmail.com"})
+        const ashley = new User({first_name:"Ashley", last_name: "Adonis", date_of_birth: 1991-04-05, email: "ashley.adonis@gmail.com"})
         return ashley.save()
     }).then(() => {
-        const robert = new User({first_name:"Robert", last_name: "Brown", date_of_birth: 12/06/1970, email: "rbrown@gmail.com"})
+        const robert = new User({first_name:"Robert", last_name: "Brown", date_of_birth: 1970-06-12, email: "rbrown@gmail.com"})
         return robert.save()
     }).then(() => {
-        const jay = new User({first_name:"Jay", last_name: "Arthur", date_of_birth: 01/02/1968, email: "jarthur@gmail.com"}) 
+        const jay = new User({first_name:"Jay", last_name: "Arthur", date_of_birth: 1968-01-02, email: "jarthur@gmail.com"}) 
         return jay.save()
     }).then(() => {
-        const zeenith = new User({first_name:"Zeenith", last_name: "Muller", date_of_birth: 01/02/1978, email: "zee.muller@gmail.com"})
+        const zeenith = new User({first_name:"Zeenith", last_name: "Muller", date_of_birth: 1978-01-01, email: "zee.muller@gmail.com"})
         return zeenith.save()
     }).then(() => {
-        const barret = new User({first_name:"Barret", last_name: "Owens", date_of_birth: 07/10/1983, email: "bowens@gmail.com"})
+        const barret = new User({first_name:"Barret", last_name: "Owens", date_of_birth: 1983-10-07, email: "bowens@gmail.com"})
         return barret.save()
     }).then(() => {
-        const jaclyn = new User({first_name:"Jaclyn", last_name: "Smith", date_of_birth: 01/02/1958, email: "jsmith@gmail.com"})
+        const jaclyn = new User({first_name:"Jaclyn", last_name: "Smith", date_of_birth: 1958-02-01, email: "jsmith@gmail.com"})
         return jaclyn.save()
     })
     
--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const User = new Schema({
     first_name: String,
     last_name: String,
-    date_of_birth: Date(),
+    date_of_birth: Date(YYYY-MM-DD),
     email: String
     
 })
