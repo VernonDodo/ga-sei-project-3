83d0cf856e6653de31d8db13b17183057ecc17b0
VernonDodo <vcdodo@gmail.com>
2019-05-20 10:16:59 -0400

    Fixed up routing in the server file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const logger = require('morgan')
 const app = express()
+const routes = require('./routes/index')
 
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`${__dirname}/client/build`));
+app.use('/', routes);
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
