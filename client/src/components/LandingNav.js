31ba9c08f027c1642fc183a1fb2e1929f832d7b8
VernonDodo <vcdodo@gmail.com>
2019-05-21 14:13:32 -0400

    Building the navigation component

--- client/src/components/LandingNav.js
+++ client/src/components/LandingNav.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Registration from './Registration';
+import About from './About';
+
 
 class LandingNav extends Component {
     render () {
@@ -9,11 +13,21 @@ class LandingNav extends Component {
             color: "white"
         }
         return(
-            
-            <div style={landingNavStyle}>
-                <span><a href="#">I am of legal drinking age. Let me in.</a></span>
+            <Router>
+                <div style={landingNavStyle}>
+                    <nav classname="navbar">
+                        <Link to="/">Home</Link>
+                        <Link to="/">Register</Link>
+                        <Link to="/">About</Link> 
+                    </nav>
+                    <Switch>
+                        <Route exact path="/home" component={Home}>
+                        <Route exact path="/registration" component={Registration}>
+                        <Route exact path="/about" component={About}>
+                    </Switch>
+                </div>
                 
-            </div>
+            </Router>
             
             
         )
