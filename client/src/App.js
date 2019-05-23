f92c0b0e2e5e20fefc41048cc455b823356be9a9
VernonDodo <vcdodo@gmail.com>
2019-05-22 22:32:27 -0400

    Changed the landing page to be responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/Landing'
 
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <LandingPage/>
-      </div>
+      <Router>
+        <div>
+          <LandingPage/>
+        </div>
+        <Switch>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/registration" component={Registration}/>
+          <Route exact path="/about" component={About}/>
+        </Switch>
+      </Router>
     )
   }
 }
